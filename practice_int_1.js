72f2187f34d382017405dcb05866f583e1b65e1b
Carina Kan <carina.d.kan@gmail.com>
2020-06-21 17:47:36 -0700

    Add practice pairboarding file

---
+++ practice_int_1.js
@@ -0,0 +1,10 @@
+// Assumptions/clarifications
+// Input: no duplicates in input array
+// Output: order of permutations doesn't matter
+// input empty, output empty
+
+// Initial thoughts
+// Brute force method: O(n * (n-1)) -> O(n^2)
+		// less time efficient, more space eff
+// Better method: recursive function
+	// more time efficient, less space eff
